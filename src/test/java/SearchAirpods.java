cfb366d0fc2df881ea6d2a92c8459f22cddf677c
Karthickkumarb <Karthickkumarb04@gmail.com>
2024-09-19 21:12:47 +0530

    Searching of Apple Airpods is done

--- src/test/java/SearchAirpods.java
+++ src/test/java/SearchAirpods.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.security.Key;
 
 public class SearchAirpods {
     WebDriver webDriver;
